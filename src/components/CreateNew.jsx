d1aa27e9b9f83bb83b11dbdb69384370ed6e78aa
Philemon Kasereka <kmboghaphilemon@gmail.com>
2026-07-24 15:26:14 +0300

    feat: implement exercise 7.3 useField reset with spread-safe destructuring

--- src/components/CreateNew.jsx
+++ src/components/CreateNew.jsx
@@ -2,9 +2,9 @@ import { useNavigate } from "react-router-dom";
 import { useField } from "../hooks";
 
 const CreateNew = ({ addAnecdote }) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
+  const { reset: resetContent, ...content } = useField("text");
+  const { reset: resetAuthor, ...author } = useField("text");
+  const { reset: resetInfo, ...info } = useField("text");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -19,9 +19,9 @@ const CreateNew = ({ addAnecdote }) => {
   };
 
   const handleReset = () => {
-    content.reset();
-    author.reset();
-    info.reset();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
@@ -30,30 +30,15 @@ const CreateNew = ({ addAnecdote }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            type={content.type}
-            value={content.value}
-            onChange={content.onChange}
-          />
+          <input name="content" {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            type={author.type}
-            value={author.value}
-            onChange={author.onChange}
-          />
+          <input name="author" {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            type={info.type}
-            value={info.value}
-            onChange={info.onChange}
-          />
+          <input name="info" {...info} />
         </div>
         <button>create</button>
         <button type="button" onClick={handleReset}>
